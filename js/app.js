9912c584ddaba0e0b18dd5bdb8b10f1075f4e086
Adrian Runceanu <adrian.runceanu@gmail.com>
2026-03-28 07:55:53 +0200

    init: structura initiala proiect DevConnect Static

---
+++ js/app.js
@@ -0,0 +1,13 @@
+js/app.js - comentariu initial
+/*
+  app.js - JavaScript-ul aplicatiei DevConnect Static
+  Vom scrie aici:
+  - Datele speakerilor si workshopurilor (hardcodate)
+  - Randarea dinamica a cardurilor
+  - Validarea si trimiterea formularului de contact
+  - Functionalitati interactive
+  Vom completa acest fisier in Laboratorul 4
+*/
+
+// Verifica ca fisierul este incarcat corect
+console.log("DevConnect app.js incarcat cu succes!");
